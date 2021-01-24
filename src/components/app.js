5822b8c766daf71638c460a87be02e59ddc01228
csaba.valyi1@gmail.com <csaba.valyi1@gmail.com>
2021-01-24 11:36:41 +0100

    Init for medium pages

--- src/components/app.js
+++ src/components/app.js
@@ -10,32 +10,33 @@ import Blog from '../routes/blog';
 import Contact from '../routes/contact';
 import ContactSuccess from '../routes/contact-success';
 import NotFoundPage from '../routes/notfound';
+import Medium from '../routes/medium';
 
 export default class App extends Component {
+  /** Gets fired when the route changes.
+   *  @param {Object} event    "change" event from [preact-router](http://git.io/preact-router)
+   *  @param {string} event.url  The newly routed URL
+   */
+  handleRoute = e => {
+    this.currentUrl = e.url;
+  };
 
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
-
-	render(props) {
-		return (
-			<Provider value={props}>
-				<div id="app">
-					<Header />
-					<Router onChange={this.handleRoute}>
-						<Home path="/" />
-						<Blogs path="/blogs/" />
-						<Blog path="/blog/:name" />
-						<Contact path="/contact/" />
-						<ContactSuccess path="/contact/success" />
-						<NotFoundPage type="404" default />
-					</Router>
-				</div>
-			</Provider>
-		);
-	}
+  render(props) {
+    return (
+      <Provider value={props}>
+        <div id="app">
+          <Header />
+          <Router onChange={this.handleRoute}>
+            <Home path="/" />
+            <Blogs path="/blogs/" />
+            <Blog path="/blog/:name" />
+            <Contact path="/contact/" />
+            <ContactSuccess path="/contact/success" />
+            <Medium path="/medium" />
+            <NotFoundPage type="404" default />
+          </Router>
+        </div>
+      </Provider>
+    );
+  }
 }
---
+++ src/routes/medium/index.js
@@ -0,0 +1,10 @@
+import { h } from 'preact';
+
+const Medium = (props) => {
+  console.log(props);
+  return (
+    <div>Medium post</div>
+  );
+};
+
+export default Medium;
