c3f27af7bc4951ae8678d129fd736dab818a9c08
csaba.valyi1@gmail.com <csaba.valyi1@gmail.com>
2021-01-24 11:09:40 +0100

    Modifying admin page template

--- src/routes/blogs/index.js
+++ src/routes/blogs/index.js
@@ -1,4 +1,4 @@
-import { h } from 'preact';
+import { h, Fragment } from 'preact';
 import { Link } from 'preact-router';
 import { usePrerenderData } from '@preact/prerender-data-provider';
 import style from './style';
@@ -27,23 +27,23 @@ function getBlogsListing(data, isLoading) {
 	if (data && data.data) {
 		const { data: blogs } = data;
 		return (
-			<>
+			<Fragment>
 				{blogs.edges.map(blog => (
-				<Link href={`/blog/${blog.id}`}>
-					<article>
-						<h2>{blog.details.title}</h2>
-						<div>
-							{
-								(blog.details.tags.substr(1, blog.details.tags.length - 2).split(',') || []).map(tag => <span class={style.tag}>{tag}</span>)
-							}
-						</div>
-						<p class={style.preview}>
-							{blog.preview}
-						</p>
-					</article>
-				</Link>
-			))}
-			</>
+					<Link href={`/blog/${blog.id}`}>
+						<article>
+							<h2>{blog.details.title}</h2>
+							<div>
+								{
+									(blog.details.tags.substr(1, blog.details.tags.length - 2).split(',') || []).map(tag => <span class={style.tag}>{tag}</span>)
+								}
+							</div>
+							<p class={style.preview}>
+								{blog.preview}
+							</p>
+						</article>
+					</Link>
+				))}
+			</Fragment>
 		);
 	}
 }
